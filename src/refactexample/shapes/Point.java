07baa8df313d8ab3d6c3dc0992d288f3954bcb4a
amerharb <amer.harb@gmail.com>
2016-05-30 20:20:40 +0200

    encapsulation

--- src/refactexample/shapes/Point.java
+++ src/refactexample/shapes/Point.java
@@ -4,6 +4,8 @@
  */
 package refactexample.shapes;
 
+import java.beans.PropertyChangeSupport;
+
 /**
  *
  * @author admin
@@ -11,12 +13,34 @@ package refactexample.shapes;
 public class Point
 {
 
-    public int x;
-    public int y;
+    private int x;
+    private int y;
 
     public Point(int x, int y)
     {
         this.x = x;
+        this.y = y;
+    }
+
+    public int getX()
+    {
+        return x;
+    }
+
+    public void setX(int x)
+    {
+        int oldX = this.x;
+        this.x = x;
+    }
+
+    public int getY()
+    {
+        return y;
+    }
+
+    public void setY(int y)
+    {
+        int oldY = this.y;
         this.y = y;
     }
 }
